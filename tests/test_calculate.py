8e97e53d8b5544326f4318818c0d2d03d362ae37
Reenes <144921239+Reetsuki@users.noreply.github.com>
2024-11-23 18:35:56 +0300

    Update test_calculate.py

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -7,39 +7,32 @@ class testcalculate(unittest.TestCase):
         fig = 'circle'
         func = 'area'
         size = [9]
-        expect = math.pi * 9 * 9
-        self.assertEqual(calc(fig, func, size), expect)
+        self.assertAlmostEqual(calc(fig, func, size), 254.4690049, 7)
     def test_calc_circle_perimeter_valid(self):
         fig = 'circle'
         func = 'perimeter'
         size = [4]
-        expect = 2 * math.pi * 4
-        self.assertEqual(calc(fig, func, size), expect)
+        self.assertAlmostEqual(calc(fig, func, size), 25.1327412, 7)
     def test_calc_square_area_valid(self):
         fig = 'square'
         func = 'area'
         size = [7]
-        expect = 7 * 7
-        self.assertEqual(calc(fig, func, size), expect)
+        self.assertEqual(calc(fig, func, size), 49)
     def test_calc_square_perimeter_valid(self):
         fig = 'square'
         func = 'perimeter'
         size = [8]
-        expect = 4 * 8
-        self.assertEqual(calc(fig, func, size), expect)
+        self.assertEqual(calc(fig, func, size), expect, 32)
     def test_calc_triangle_area_valid(self):
         fig = 'triangle'
         func = 'area'
         size = [8, 6, 5]
-        p = (8 + 6 + 5) / 2
-        expect = math.sqrt(p * (p - 8) * (p - 6) * (p - 5))
-        self.assertEqual(calc(fig, func, size), expect)
+        self.assertAlmostEqual(calc(fig, func, size), 14.9812383, 7)
     def test_calc_triangle_perimeter_valid(self):
         fig = 'triangle'
         func = 'perimeter'
         size = [6, 5, 6]
-        expect = 6 + 5 + 6
-        self.assertEqual(calc(fig, func, size), expect)
+        self.assertEqual(calc(fig, func, size), 17)
     def test_calc_raises_error_for_figure_invalid(self):
         fig = 'notfig'
         func = 'area'
