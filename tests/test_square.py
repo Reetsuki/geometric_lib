4d808ce127a223f875448157a4b70a4386139082
Reenes <144921239+Reetsuki@users.noreply.github.com>
2024-11-23 18:43:25 +0300

    Update test_square.py

--- tests/test_square.py
+++ tests/test_square.py
@@ -4,7 +4,7 @@ from square import area, perimeter
 class testsquare(unittest.TestCase):
     def test_area(self):
         side = 6
-        self.assertEqual(area(6), 6 * 6)
+        self.assertEqual(area(6), 36)
     def test_perimeter(self):
         side = 8
-        self.assertEqual(perimeter(8), 4 * 8)
+        self.assertEqual(perimeter(8), 32)
